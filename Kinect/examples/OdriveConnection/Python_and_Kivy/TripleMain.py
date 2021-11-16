8c975ac05f6f5ae4095e6affa424455e66870a8f
nikhi1g <2nikhilg@gmail.com>
2021-11-16 11:44:34 -0800

    Working with kivy, timer, and names

--- Kinect/examples/OdriveConnection/Python_and_Kivy/TripleMain.py
+++ Kinect/examples/OdriveConnection/Python_and_Kivy/TripleMain.py
@@ -1,4 +1,5 @@
 # This is a sample Python script.
+# This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -11,7 +12,7 @@ import odrive
 # buttons are actually buttons -1
 # from pidev.Joystick import Joystick
 from ODrive_Ease_Lib import *
-
+#import main#never do this!?
 # variables
 # findodrive
 odboard = odrive.find_any(serial_number='208D3388304B')
@@ -58,6 +59,7 @@ def timer_update():
     global seconds
     global TimerRunning
     while TimerRunning:
+        # print ("Timer On")
         print(seconds)
         print(' ')
         print(' ')
@@ -66,15 +68,161 @@ def timer_update():
 
 
 def Kivy_Update():
-    print("You have reached a new high score, enter your name:")
+
     print("Seconds Passed:", seconds)
-    file = open('../Testers_and_Libraries/secondsstorage.txt', 'a')
-    file.write(str(seconds))
+    file = open('storage.txt', 'a')
+    file.write('\n'+str(seconds) + '')
+    # Call the naming function? No it does not.
+
+
     file.close()
     global TimerRunning
     TimerRunning = False
 
+
     # https://stackoverflow.com/questions/56711424/how-can-i-count-time-in-python-3
+
+
+def odrive_and_kinect_startup():
+    global KinectIsOn
+    # dumperrors
+    dump_errors(odboard)
+    # define ax and ay
+    ax = ODrive_Axis(odboard.axis0, 15, 10)  # currentlim, vlim
+    ay = ODrive_Axis(odboard.axis1, 40, 16)  # currentlim, vlim
+    az = ODrive_Axis(odboard1.axis0, 15, 10)
+    # calibrate
+    if not ax.is_calibrated():  # calibrate x (left right)
+        print("calibrating x...")
+        ax.calibrate_with_current_lim(25)
+    if not az.is_calibrated():  # calibrate z (left right)
+        print("calibrating z...")
+        az.calibrate_with_current_lim(25)
+
+    ax.gainz(20, 0.16, 0.32, False)
+    az.gainz(20, 0.16, 0.32, False)
+    odboard.clear_errors()
+
+    ax.idle()
+    ay.idle()
+    az.idle()
+    axisshortcut = az.axis
+    # homing_sensor_prox
+    odboard1.config.gpio8_mode = GPIO_MODE_DIGITAL
+    axisshortcut.min_endstop.config.gpio_num = 2  # pin 8 for x, 2 for y, 2 for z
+    Prox_Sensor_Number = axisshortcut.min_endstop.config.gpio_num  # setting to global class Runner variable just so that I can reference it in the Thread print statements
+    axisshortcut.min_endstop.config.enabled = True  # Turns sensor on, says that I am using it
+    axisshortcut.min_endstop.config.offset = 1  # stops 1 rotation away from sensor
+    axisshortcut.min_endstop.config.debounce_ms = 20  # checks again after 20 milliseconds if actually pressed, which is what debounce is :D
+    axisshortcut.min_endstop.config.offset = -1.0 * (
+        8192)  # hop back from GPIO in order to allow for function again
+    odboard1.config.gpio8_mode = GPIO_MODE_DIGITAL_PULL_DOWN
+
+    sleep(10)
+
+    # initilize the Kinect
+
+    # Initialize the library, if the library is not found, add the library path as argument
+    pykinect.initialize_libraries(module_k4abt_path="/usr/lib/libk4abt.so", track_body=True)
+
+    # Modify camera configuration
+    device_config = pykinect.default_configuration
+    device_config.color_resolution = pykinect.K4A_COLOR_RESOLUTION_OFF
+    device_config.depth_mode = pykinect.K4A_DEPTH_MODE_WFOV_2X2BINNED
+    # print(device_config)
+
+    # Start device
+    device = pykinect.start_device(config=device_config)
+
+    # Start body tracker
+    bodyTracker = pykinect.start_body_tracker()
+
+    # cv2.namedWindow('Depth image with skeleton', cv2.WINDOW_NORMAL)
+    # run the Kinect
+    capture_list = []
+
+    # START TIM
+    Thread(target=timer_update).start()
+    while True:
+
+        # Get capture
+        capture = device.update()
+
+        # Get body tracker frame
+        body_frame = bodyTracker.update()  # LOOK HERE
+
+        if KinectIsOn:
+
+
+            body_list = [body_frame.get_body_skeleton(body_num) for body_num in
+                         range(body_frame.get_num_bodies())]  # le code
+            try:
+                close_body = min(body_list, key=lambda body: body.joints[
+                    26].position.xyz.z)  # grabs the minimum body according to the head z depth
+            except ValueError:
+                close_body = None
+
+            if close_body != None:
+                rightz = close_body.joints[15].position.xyz.z
+                leftz = close_body.joints[8].position.xyz.z
+
+                righthand = close_body.joints[15].position.xyz.x
+                lefthand = close_body.joints[8].position.xyz.x
+
+                righthandy = close_body.joints[15].position.xyz.y
+                lefthandy = close_body.joints[8].position.xyz.y
+
+                if lefthandy < -400 and righthandy > -100:
+                    print("to the LEFT!")
+                    ax.set_vel(3)
+
+                if lefthandy > -100 and righthandy < -400:
+                    print("to the RIGHT!")
+                    ax.set_vel(-3)
+
+                if -400 < lefthandy < -100 and -400 < righthandy < -100:
+                    print("in the MIDDLE!")
+                    ax.set_vel(0)
+                    ay.set_vel(0)
+                    # stop if leave the frame
+                if righthand < -700 or righthand > 700:
+                    print("Stopping because righthand left the frame")
+                    ax.set_vel(0)
+
+                if lefthand < -700 or lefthand > 700:
+                    print("Stopping because lefthand left the frame")
+                    ax.set_vel(0)
+
+
+                print("RightZ:", rightz)
+                print("LeftY:", lefthandy)
+                print("RightY:", righthandy)
+
+
+            _k4a.k4a_capture_release(capture.handle())
+            _k4abt.k4abt_frame_release(body_frame.handle())
+
+
+
+            if cv2.waitKey(1) == ord('q'):
+                ax.set_vel(0)
+                ax.idle()
+                break
+
+                # append the images to the array. if the array is == 3000, then run a whilie loop that clear the array according the handle's id.
+
+            if az.axis.min_endstop.endstop_state:  # if switch, or in this case the prox is pressed
+                # clear errors will get rid of freeze man
+                print('Reached Prox on GPIO', Prox_Sensor_Number)
+                dump_errors(odboard1)
+                odboard1.clear_errors()
+                ax.set_vel(0)
+                az.set_vel(0)
+                sleep(3)
+                Thread(target=Kivy_Update).start()
+                sleep(1)
+                KinectIsOn = False
+
 
 
 if __name__ == '__main__':
@@ -92,12 +240,7 @@ if __name__ == '__main__':
     if not az.is_calibrated():  # calibrate z (left right)
         print("calibrating z...")
         az.calibrate_with_current_lim(25)
-    # if not self.ay.is_calibrated(): #calibrate y (top bottom)
-    #     print("calibrating y...")
-    #     self.ay.calibrate_with_current(60)
-    # odboard.save_configuration()
-    # odboard.reboot()
-    # COMMENCE THE GAINZ
+
     ax.gainz(20, 0.16, 0.32, False)
     az.gainz(20, 0.16, 0.32, False)
     odboard.clear_errors()
@@ -141,6 +284,8 @@ if __name__ == '__main__':
     capture_list = []
 
     Thread(target=timer_update).start()  # START TIM
+
+
     while True:
 
         # Get capture
@@ -149,42 +294,8 @@ if __name__ == '__main__':
         # Get body tracker frame
         body_frame = bodyTracker.update()  # LOOK HERE
 
-        # Get the color depth image from the capture
-        # ret, depth_color_image = capture.get_colored_depth_image()
 
-        # Get the colored body segmentation
-        # ret, body_image_color = body_frame.get_segmentation_image()
-
-        # if not ret:
-        #     continue
-
-        # Combine both images
-        # combined_image = cv2.addWeighted(depth_color_image, 0.6, body_image_color, 0.4, 0)
-
-        # Draw the skeletons
-        # combined_image = body_frame.draw_bodies(combined_image)  # LOOK HERE
-
-        # Overlay body segmentation on depth image
-        # cv2.imshow('Depth image with skeleton', combined_image)
         if KinectIsOn:
-            # print(body_frame.get_segmentation_image())
-
-            # raised hand around -1000~1000 units
-
-            # print("Right Hand X:", body_frame.get_body_skeleton().joints[15].position.xyz.x)
-            # body_num = body_frame.get_num_bodies()
-            # close_body = 0
-            # for body_num in range(body_frame.get_num_bodies()):
-            #
-            #     print("body", body_num, body_frame.get_body_skeleton(body_num).joints[26].position.xyz.z)
-            #     if body_num == 0:
-            #         pass
-            #     else:
-            #         if body_frame.get_body_skeleton(close_body).joints[26].position.xyz.z > body_frame.get_body_skeleton(body_num).joints[26].position.xyz.z:
-            #             print("Current body is closest")
-            #             close_body = body_num
-            #         else:
-            #             print("Previous body is closest")
 
             body_list = [body_frame.get_body_skeleton(body_num) for body_num in
                          range(body_frame.get_num_bodies())]  # le code
@@ -224,56 +335,17 @@ if __name__ == '__main__':
                 if lefthand < -700 or lefthand > 700:
                     print("Stopping because lefthand left the frame")
                     ax.set_vel(0)
-                #
-                # #both hands are raised
-                # if lefthandy < -200 and righthandy < -200:
-                #     print("upwards")
-                #     az.set_vel(4)
-                # #both hands are lowered
-                # if lefthandy > 200 and righthandy > 200:
-                #     print("downwards")
-                #     az.set_vel(-4)
+
 
                 print("RightZ:", rightz)
                 print("LeftY:", lefthandy)
                 print("RightY:", righthandy)
 
-            # IT WORKS! TILTING MOVES IT!!!
-            # yooo this is lit
 
-            # golden
-
-            # golden
-
-            # try to return the nunbodies within 900
-
-            # depthtesting
-
-            # if leftz > 1300:
-            #     print("Up?")
-            #     ay.set_ramped_vel(.8, .5)
-            #
-            # if leftz < 800:
-            #     print("Down?")
-            #     ay.set_ramped_vel(-.8, .5)
-            #
-            # if 800 < leftz < 1300:
-            #     print("Stop?")
-            #     ay.set_ramped_vel(0, .5)
-
-            # print("Left", leftz)
-            # print("")
-            # print("Right", rightz)
             _k4a.k4a_capture_release(capture.handle())
             _k4abt.k4abt_frame_release(body_frame.handle())
 
-            # numbxerbodies = []
-            # min = None
-            #
-            # for bodies in numberbodies:
-            # body_frame.get_body_skeleton().joints[26].position.xyz.z = bodies()
-            # get.z.skel1
-            # get.z.skel2
+
 
             if cv2.waitKey(1) == ord('q'):
                 ax.set_vel(0)
--- Kinect/examples/OdriveConnection/Python_and_Kivy/main.py
+++ Kinect/examples/OdriveConnection/Python_and_Kivy/main.py
@@ -1,4 +1,338 @@
 import TripleMain
+# os.environ['DISPLAY'] = ":0.0"
+# os.environ['KIVY_WINDOW'] = 'egl_rpi'
+from kivy.properties import ObjectProperty
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from threading import Thread
+from time import sleep
+from pidev.kivy import DPEAButton
+
+# from datetime import datetime
+
+MAIN_SCREEN_NAME = 'main'
+TIMER_SCREEN_NAME = 'TimerScreen'
+WAIT_SCREEN_NAME = 'WaitScreen'
+SCREEN_MANAGER = ScreenManager()
+
+
+class KinectGUI(App):
+    # class that runs the gui
+    def build(self):
+        # creates the window
+        return SCREEN_MANAGER
+
+
+Window.clearcolor = (1, 1, 1, 1)  # sets the window color to white
+
+
+class MainScreen(Screen):
+    # class that handles the main events and stores objects from kivy file
+    name_enter = ["a"]
+    index = 0
+
+    a1 = ObjectProperty(None)
+    b1 = ObjectProperty(None)
+    c1 = ObjectProperty(None)
+    d1 = ObjectProperty(None)
+    e1 = ObjectProperty(None)
+    f1 = ObjectProperty(None)
+    g1 = ObjectProperty(None)
+    h1 = ObjectProperty(None)
+    i1 = ObjectProperty(None)
+    j1 = ObjectProperty(None)
+    k1 = ObjectProperty(None)
+    l1 = ObjectProperty(None)
+    m1 = ObjectProperty(None)
+    n1 = ObjectProperty(None)
+    o1 = ObjectProperty(None)
+    p1 = ObjectProperty(None)
+    q1 = ObjectProperty(None)
+    r1 = ObjectProperty(None)
+    s1 = ObjectProperty(None)
+    t1 = ObjectProperty(None)
+    u1 = ObjectProperty(None)
+    v1 = ObjectProperty(None)
+    w1 = ObjectProperty(None)
+    x1 = ObjectProperty(None)
+    y1 = ObjectProperty(None)
+    z1 = ObjectProperty(None)
+    space = ObjectProperty(None)
+    star = ObjectProperty(None)
+    dash = ObjectProperty(None)
+    delete = ObjectProperty(None)
+    enter = ObjectProperty(None)
+    timer = ObjectProperty(None)
+
+    # making a file that appends each new name when it is entered:
+
+    # this works, now how to add each line to an array so that it can be read and a leaderboard can be made. #burh the v key is missing
+    fileonopen = open('namestorage.txt', 'w')
+    fileonopen.write(' ')
+    fileonopen.close()
+
+    def A_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.a1.text))
+        file.close()
+        self.display_name()
+
+    def B_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.b1.text))
+        file.close()
+        self.display_name()
+
+    def C_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.c1.text))
+        file.close()
+        self.display_name()
+
+    def D_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.d1.text))
+        file.close()
+        self.display_name()
+
+    def E_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.e1.text))
+        file.close()
+        self.display_name()
+
+    def F_Key_Update(self):
+        file = open('namestorage.txt', 'a')  #
+        file.write(str(self.f1.text))
+        file.close()
+        self.display_name()
+
+    def G_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.g1.text))
+        file.close()
+        self.display_name()
+
+    def H_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.h1.text))
+        file.close()
+        self.display_name()
+
+    def I_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.i1.text))
+        file.close()
+        self.display_name()
+
+    def J_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.j1.text))
+        file.close()
+        self.display_name()
+
+    def K_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.k1.text))
+        file.close()
+        self.display_name()
+
+    def L_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.l1.text))
+        file.close()
+        self.display_name()
+
+    def M_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.m1.text))
+        file.close()
+        self.display_name()
+
+    def N_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.n1.text))
+        file.close()
+        self.display_name()
+
+    def O_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.o1.text))
+        file.close()
+        self.display_name()
+
+    def P_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.p1.text))
+        file.close()
+        self.display_name()
+
+    def Q_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.q1.text))
+        file.close()
+        self.display_name()
+
+    def R_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.r1.text))
+        file.close()
+        self.display_name()
+
+    def S_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.s1.text))
+        file.close()
+        self.display_name()
+
+    def T_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.t1.text))
+        file.close()
+        self.display_name()
+
+    def U_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.u1.text))
+        file.close()
+        self.display_name()
+
+    def V_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.v1.text))
+        file.close()
+        self.display_name()
+
+    def W_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.w1.text))
+        file.close()
+        self.display_name()
+
+    def X_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.x1.text))
+        file.close()
+        self.display_name()
+
+    def Y_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.y1.text))
+        file.close()
+        self.display_name()
+
+    def Z_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.z1.text))
+        file.close()
+        self.display_name()
+
+    def SPACE_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.space.text))
+        file.close()
+        self.display_name()
+
+    def STAR_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.star.text))
+        file.close()
+        self.display_name()
+
+    def DASH_Key_Update(self):
+        file = open('namestorage.txt', 'a')
+        file.write(str(self.dash.text))
+        file.close()
+        self.display_name()
+
+    def ENTER_Key_Update(self):
+        file = open('namestorage.txt', 'r')
+        name = file.read()
+        file.close()
+        file = open('storage.txt', 'a')
+        file.write(name)
+        file.write('\n')
+        file.close()
+        sleep(0.4)
+        SCREEN_MANAGER.current = WAIT_SCREEN_NAME
+
+
+    def DELETE_Key_Update(self):  # this WORKS omg
+        with open('namestorage.txt', 'rb+') as f:
+            f.seek(0, 2)  # end of file
+            size = f.tell()  # the size...
+            f.truncate(size - 1)  # truncate at that size - how ever many characters
+            self.display_name()
+
+
+    def display_name(self):
+        file = open('namestorage.txt', 'r')
+        name = file.read()
+        file.close()
+        self.timer.text = name
 
 
 
+class WaitScreen(Screen):
+    def __init__(self, **kwargs):
+        super(WaitScreen, self).__init__(**kwargs)
+
+    loading = ObjectProperty(None)
+
+class TimerScreen(Screen):
+
+    def __init__(self, **kwargs):
+        super(TimerScreen, self).__init__(**kwargs)
+
+    timer = ObjectProperty(None)
+    timer1 = ObjectProperty(None)
+
+    # update the label of timer to be the seconds that have been passed
+
+    def go_to_keyboard(self):
+        SCREEN_MANAGER.current = MAIN_SCREEN_NAME
+
+    def timer_label_update(self):
+        while True:
+            self.timer.text = "Time passed: " + str(get_seconds()-13)
+            sleep(0.5)
+            if TripleMain.KinectIsOn == False:
+                SCREEN_MANAGER.current = MAIN_SCREEN_NAME
+                break
+
+    def timer_label_update_thread(self):
+        Thread(target=self.timer_label_update).start()
+
+
+
+def everything_start():
+    TripleMain.odrive_and_kinect_startup()
+
+
+def get_seconds():
+    while True:
+        return TripleMain.seconds
+
+
+Builder.load_file('TimerScreen.kv')
+Builder.load_file('main.kv')
+Builder.load_file('WaitScreen.kv')
+SCREEN_MANAGER.add_widget(TimerScreen(name=TIMER_SCREEN_NAME))
+SCREEN_MANAGER.add_widget(MainScreen(name=MAIN_SCREEN_NAME))
+SCREEN_MANAGER.add_widget(WaitScreen(name=WAIT_SCREEN_NAME))
+SCREEN_MANAGER.current = TIMER_SCREEN_NAME
+
+
+
+if __name__ == '__main__':
+    Thread(target=everything_start).start()
+    KinectGUI().run()
+
+# stackeeeeeexchange
+# file = 'namestorage.txt'
+# deleteeomthing___________(file, 1)#does this even work? #https://stackoverflow.com/questions/18857352/remove-very-last-character-in-file
+
+
+# https://stackoverflow.com/questions/56711424/how-can-i-count-time-in-python-3
